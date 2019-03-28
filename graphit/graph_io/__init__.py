b47fc5ddf46e2c502b0c3ae68aeb38f0cfe3733e
Marc van Dijk <marcvdijk@gmail.com>
2019-03-28 16:55:30 +0100

    Remove IO module level imports

--- graphit/graph_io/__init__.py
+++ graphit/graph_io/__init__.py
@@ -6,20 +6,3 @@ These include both data structures in a format dedicated to representing graphs
 and other (hierarchical) data that has a structure that could be represented as
 a graph.
 """
-
-# Dedicated graph formats
-from graphit.graph_io.io_dot_format import *
-from graphit.graph_io.io_jgf_format import *
-from graphit.graph_io.io_pgf_format import *
-from graphit.graph_io.io_tgf_format import *
-
-# General (hierarchical) data structures
-from graphit.graph_io.io_json_format import *
-from graphit.graph_io.io_pydata_format import *
-from graphit.graph_io.io_web_format import *
-from graphit.graph_io.io_xml_format import *
-from graphit.graph_io.io_yaml_format import *
-
-# Specific data formats
-from graphit.graph_io.io_cwl_format import *
-from graphit.graph_io.io_jsonschema_format import *
