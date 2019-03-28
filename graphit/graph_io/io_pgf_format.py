6880e0ad4084ad3c52085703799c4c6adf57614b
Marc van Dijk <marcvdijk@gmail.com>
2019-03-28 17:13:07 +0100

    Fix PGF import/export and add pickle support

--- graphit/graph_io/io_pgf_format.py
+++ graphit/graph_io/io_pgf_format.py
@@ -8,74 +8,111 @@
 Reading and writing graphs defined in Propitiatory Graph Format
 (.pgf) a format specific to the graphit module.
 
-Graph nodes, edges and adjacency are stored as plain python dictionaries
+Graph nodes, edges and data are stored as plain python dictionaries
 """
 
-import os
 import pprint
 import logging
+import pickle
+import ast
 
 from graphit import Graph, __module__
+from graphit.graph_exceptions import GraphitException
+from graphit.graph_io.io_helpers import open_anything
 from graphit.graph_py2to3 import StringIO
 
-__all__ = ['read_graph', 'write_graph']
+__all__ = ['read_pgf', 'write_pgf']
 logger = logging.getLogger(__module__)
 
 
-def read_graph(graph_file, graph=None):
+def read_pgf(pgf_file, graph=None, pickle_graph=False):
     """
     Import graph from Graph Python Format file
 
-    GPF format is the modules own file format consisting out of a serialized
-    nodes and edges dictionary.
+    PGF format is the modules own file format consisting out of a serialized
+    graph data, nodes and edges dictionaries. Import either as plain text
+    serialized dictionary or pickled graph object.
     The format is feature rich with good performance but is not portable.
 
-    :param graph_file:    File path to read from
-    :type graph_file:     :py:str
+    :param pgf_file:      PGF data to parse
+    :type pgf_file:       File, string, stream or URL
     :param graph:         Graph object to import to or Graph by default
     :type graph:          :graphit:Graph
+    :param pickle_graph:  PGF format is a pickled graph
+    :type pickle_graph:   :py:bool
 
     :return:              Graph instance
     :rtype:               :graphit:Graph
     """
 
+    # Unpickle pickled PGF format
+    if pickle_graph:
+        pgf_file = open_anything(pgf_file, mode='rb')
+        pgraph = pickle.load(pgf_file)
+
+        # Transfer data from unpickled graph to graph if defined
+        if graph:
+            graph.origin.nodes, graph.origin.edges, graph.origin.adjacency, graph.origin.data = graph.storagedriver(
+                pgraph.nodes, pgraph.edges, pgraph.data)
+            return graph
+        return pgraph
+
+    # Import graph from serialized Graph Python Format
     if not graph:
         graph = Graph()
 
-    # Import graph from serialized Graph Python Format
-    with open(graph_file) as f:
-        code = compile(f.read(), "GPF_file", 'exec')
-        exec(code)
+    pgf_file = open_anything(pgf_file)
+    pgf_eval = ast.literal_eval(pgf_file.read())
+    if not isinstance(pgf_eval, dict):
+        raise GraphitException('Invalid PGF file format')
+
+    missing_data = [d for d in pgf_file if d not in ('data', 'nodes', 'edges')]
+    if missing_data:
+        raise GraphitException('Invalid PGF file format, missing required attributes: {0}'.format(
+            ','.join(missing_data)))
+
+    graph.origin.nodes, graph.origin.edges, graph.origin.adjacency, graph.origin.data = graph.storagedriver(
+        pgf_eval['nodes'], pgf_eval['edges'], pgf_eval['data'])
 
     return graph
 
 
-def write_graph(graph):
+def write_pgf(graph, pickle_graph=False):
     """
     Export graph as Graph Python Format file
 
-    GPF format is the modules own file format consisting out of a serialized
-    nodes and edges dictionary.
-    The format is feature rich wth good performance but is not portable.
+    PGF format is the modules own file format consisting out of a serialized
+    graph data, nodes and edges dictionaries. Exports either as plain text
+    serialized dictionary or pickled graph object.
+    The format is feature rich with good performance but is not portable.
 
     :param graph:        Graph object to export
-    :type graph:         Graph instance
+    :type graph:         :graphit:Graph
+    :param pickle_graph: serialize the Graph using Python pickle
+    :type pickle_graph:  :py:bool
 
     :return:             Graph in GPF graph representation
     :rtype:              :py:str
     """
 
-    # Create empty file buffer
-    string_buffer = StringIO()
+    # Pickle or not
+    if pickle_graph:
+        return pickle.dumps(graph)
 
     # Export graph as serialized Graph Python Format
     pp = pprint.PrettyPrinter(indent=2)
+    string_buffer = StringIO()
+    string_buffer.write('{')
+
+    # Export graph data dictionary
+    string_buffer.write('"data": {0},\n'.format(pp.pformat(graph.data.to_dict())))
 
     # Export nodes as dictionary
-    string_buffer.write('nodes = {0}\n'.format(pp.pformat(graph.nodes.dict())))
+    string_buffer.write('"nodes": {0},\n'.format(pp.pformat(graph.nodes.to_dict())))
 
     # Export edges as dictionary
-    string_buffer.write('edges = {0}\n'.format(pp.pformat(graph.edges.dict())))
+    string_buffer.write('"edges": {0},\n'.format(pp.pformat(graph.edges.to_dict())))
+    string_buffer.write('}')
 
     logger.info('Graph {0} exported in GPF format'.format(repr(graph)))
 
