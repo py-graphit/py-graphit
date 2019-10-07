93162b44d1a18a3a5e95deaa7936fc373ecc6b0d
Marc van Dijk <marcvdijk@gmail.com>
2019-10-07 08:24:21 +0200

    Update description

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ with open("README.md", "r") as fh:
 setup(
     name='py-graphit',
     version="0.2.1",
-    description='Graph based data handling for the MDStudio application',
+    description='A Python graph based data modeling library',
     long_description=long_description,
     long_description_content_type="text/markdown",
     author='Marc van Dijk - VU University - Amsterdam,',
